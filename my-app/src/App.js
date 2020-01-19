3dd51540f9475877a75544cdcdf960ac1b9c7420
JT <taubman33@gmail.com>
2020-01-18 22:18:22 -0500

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,37 +6,46 @@ import './App.css';
 
 
 // //linking the API
-const url = 'https://www.anapioficeandfire.com/' 
+// const url = 'https://anapioficeandfire.com/api/characters/583'
 
 //I guess we are keeping this functional for now? and the other will be class based?
 function App() {
   
-  // const [characters, listCharacters] = useState([])  
+  const [characters, listCharacters] = useState([])  
  
-  // //our fetch data app, working with async. I am finally feeling decent about the usage of the fat arrow
-  // const fetchData = async () => {
-  // try {
-  //   let response = await axios.get(URL)
-  //   listCharacters(response.data)
-  // } catch (err) {
-  //   console.log(`Seven Hells! We have an error at ${err}!`)
-  // }
+  //our fetch data app, working with async. I am finally feeling decent about the usage of the fat arrow
+  const fetchData = async () => {
+  try {
+    let response = await axios.get(URL)
+    listCharacters(response.data)
+  } catch (err) {
+    console.log(`Seven Hells! We have an error at ${err}!`)
+  }
 
-  // }
+  }
   
-  // //setting up the fetchData function to go 
-  // useEffect(() =>{
-  //   fetchData();
+  //setting up the fetchData function to go 
+  useEffect(() =>{
+    fetchData();
 
-  // }, [])
+  }, [])
 
 
 
   return (
     <div className="App">
+      <header>
       <h1> Game of Thrones API</h1>
-
+      </header>
+    
+    <article>
+      <p>info is going here</p>
+      <button onClick =""> Hear me Roar!</button>
+    </article>
+   
     </div>
   );
   }
+
+
   export default App;
